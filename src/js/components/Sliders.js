cf2541915bea6ed70c6fd97b4131d3d43e58e09e
nazarriko@gmail.com <NazariiDev2409>
2018-09-06 21:13:15 +0300

    initial

---
+++ src/js/components/Sliders.js
@@ -0,0 +1,42 @@
+import 'slick-carousel';
+import { svgIcon } from '../_helpers';
+
+class Sliders {
+  constructor() {
+    this.$testimonialsSld = $('.testimonials__slider');
+
+    const iconLeft = svgIcon('sld-arr-l');
+    const iconRight = svgIcon('sld-arr-r');
+
+    this.defaultOptions = {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      infinite: true,
+      speed: 800,
+      useTransform: true,
+      adaptiveHeight: true,
+      accessibility: false,
+      swipe: true,
+      arrows: true,
+      prevArrow: `<button type="button" class="slider-btn slider-btn_prev">${iconLeft}</button>`,
+      nextArrow: `<button type="button" class="slider-btn slider-btn_next">${iconRight}</button>`,
+      rows: 0
+    };
+
+    this.init();
+  }
+
+  init() {
+    if (this.$testimonialsSld.length) this.initTestimonialsSld();
+  }
+
+  initTestimonialsSld() {
+    this.$testimonialsSld.slick($.extend({}, this.defaultOptions, {
+      slidesToShow: 3,
+      infinite: false
+    }));
+  }
+}
+
+export default new Sliders();
